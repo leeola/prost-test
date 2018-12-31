dd2999d7a38b2e2da93904a41747c369fb510de7
Lee Olayvar <leeola@fastmail.com>
2018-12-31 13:17:26 -0800

    remove twirp

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-extern crate futures;
-extern crate hyper;
-extern crate prost;
 #[macro_use]
 extern crate prost_derive;
-extern crate prost_twirp;
 
 mod service {
     include!(concat!(env!("OUT_DIR"), "/foo.rs"));
